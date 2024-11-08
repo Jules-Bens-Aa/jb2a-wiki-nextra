8d7f38c0d2b2e0edb9fcbd69b412e093188e763b
Jules-Bens-Aa <73002638+Jules-Bens-Aa@users.noreply.github.com>
2024-11-08 07:00:41 +0100

    added public images and a few theme properties

--- theme.config.tsx
+++ theme.config.tsx
@@ -1,17 +1,36 @@
 import React from 'react'
-import { DocsThemeConfig } from 'nextra-theme-docs'
+import { DocsThemeConfig, useConfig } from 'nextra-theme-docs'
 
 const config: DocsThemeConfig = {
-  logo: <span>My Project</span>,
+  primaryHue: 52,
+  logo: (
+    <>
+      <img
+        src= 'images/jb2a-wiki-banner-02.png'
+        decoding='async'
+        loading='lazy'
+        style= {{ width: "200px" }}
+        alt= "JB2A"
+      />
+    </>
+  ),
   project: {
-    link: 'https://github.com/shuding/nextra-docs-template',
+    link: 'https://github.com/Jules-Bens-Aa/jb2a-wiki-nextra',
   },
   chat: {
-    link: 'https://discord.com',
+    link: 'https://discord.gg/gmd8MAPX4m',
   },
   docsRepositoryBase: 'https://github.com/shuding/nextra-docs-template',
   footer: {
     text: 'Nextra Docs Template',
+  },
+  sidebar: {
+    defaultMenuCollapseLevel: 1,
+    toggleButton: true
+  },
+  navigation: {
+    prev: true,
+    next: true
   },
 }
 
