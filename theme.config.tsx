9ca3792d9f018b5ecce8e26541fd91dd24dfc6d6
Jules-Bens-Aa <73002638+Jules-Bens-Aa@users.noreply.github.com>
2024-11-08 16:36:08 +0100

    theme colours and fix to logo image

--- theme.config.tsx
+++ theme.config.tsx
@@ -1,14 +1,14 @@
 import React from 'react'
-import { DocsThemeConfig, useConfig } from 'nextra-theme-docs'
+import { DocsThemeConfig } from 'nextra-theme-docs'
 
 const config: DocsThemeConfig = {
-  primaryHue: 52,
+  primaryHue: { dark: 45, light: 35 },
+  primarySaturation: { dark: 100, light: 50 },
+  
   logo: (
     <>
       <img
-        src= 'images/jb2a-wiki-banner-02.png'
-        decoding='async'
-        loading='lazy'
+        src= '/images/jb2a-wiki-banner-02.png'
         style= {{ width: "200px" }}
         alt= "JB2A"
       />
@@ -19,19 +19,38 @@ const config: DocsThemeConfig = {
   },
   chat: {
     link: 'https://discord.gg/gmd8MAPX4m',
+    // icon: (
+    //   <>
+    //     <img
+    //       src= 'https://static-00.iconduck.com/assets.00/discord-icon-2048x2048-nnt62s2u.png'
+    //       style= {{ width: "30px" }}
+    //       alt= "Discord-Server"
+    //     />
+    //   </>
+    // )
   },
   docsRepositoryBase: 'https://github.com/shuding/nextra-docs-template',
   footer: {
-    text: 'Nextra Docs Template',
+    text: 'JB2A wiki',
   },
   sidebar: {
     defaultMenuCollapseLevel: 1,
-    toggleButton: true
+    toggleButton: true,
+    autoCollapse: true
   },
   navigation: {
     prev: true,
     next: true
   },
+  editLink: {
+    component: null
+  },
+  feedback: {
+    content: null
+  },
+  search: {
+    placeholder: "Search the wiki!😎"
+  }
 }
 
 export default config
